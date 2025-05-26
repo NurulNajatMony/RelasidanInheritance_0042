558bd9faab83923491da3637b90be4f6fb27dd66
NurulNajatMony <nurulmony505@gmail.com>
2025-05-26 15:14:45 +0700

    menetapkan relasi antara ibu dan anak

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -11,3 +11,8 @@ int main() {
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("dewi");
+
+    varIbu1->tambahAnak(varAnak1);
+    varIbu1->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
